c0e2d7125a127a12668eb3f93f5e73178a022755
Alex <xander.zaranek@gmail.com>
2015-05-01 19:03:14 -0400

    Renamed entity
    renamed entity to health and made it a component

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+
+    private int m_maxHealth;
+    private int m_currentHealth;
+
+    public int currentHealth
+    {
+        get { return m_currentHealth; }
+        set { m_currentHealth = value; }
+    }
+
+    public int maxHealth
+    {
+        get { return m_maxHealth; }
+        set { m_maxHealth = value; }
+    }
+
+    public void TakeDamage(int p_dmg)
+    {
+        m_currentHealth -= p_dmg;
+    }
+
+
+}

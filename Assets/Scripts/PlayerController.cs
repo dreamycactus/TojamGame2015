daef23d04366b5a78ad72b2eb6c4a996d3a4e34a
Adam Bysice <adambysice@gmail.com>
2015-05-01 19:10:37 -0400

    Starting player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@
  */
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
 
@@ -15,6 +16,20 @@ public class PlayerController : MonoBehaviour {
     #endregion
 
     #region Private Variables
+    // state machine code 
+    public enum CharacterStateNames
+    {
+        NullState = 0,
+        
+
+    }
+    private Dictionary<CharacterStateNames, PlayerBase> m_gameStateDictionary = new Dictionary<CharacterStateNames, PlayerBase>();
+    private GameStateBase m_currentGameState = null;
+    private CharacterStateNames m_currentGameStateIndex = CharacterStateNames.NullState;
+    private CharacterStateNames m_nextGameStateIndex = CharacterStateNames.NullState;
+    private bool m_initialised = false;
+
+    // movement
     private GameObject m_Camera;
     private Rigidbody2D m_rb;
     
@@ -40,6 +55,9 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
+        //GetInputs
+
+
 	    
 	}
     // FixedUpdate is called on a timer
@@ -54,6 +72,10 @@ public class PlayerController : MonoBehaviour {
         {
             m_rb.AddForce(-Vector2.right * m_ForceMultiplier);
         }
+        //bugs
+
+
+
     }
 
     #endregion
@@ -66,4 +88,36 @@ public class PlayerController : MonoBehaviour {
 
     #region Private Methods
     #endregion
+}
+
+
+public abstract class PlayerBase
+{
+    protected PlayerController m_cont;
+
+    public PlayerBase()
+    {
+
+    }
+
+    public PlayerBase(PlayerController p_cont)
+    {
+        m_cont = p_cont;
+    }
+
+
+    public virtual void EnterState(PlayerController.CharacterStateNames p_prevState)
+    {
+
+    }
+
+    public virtual void UpdateState()
+    {
+
+    }
+
+    public virtual void ExitState(PlayerController.CharacterStateNames p_nextState)
+    {
+
+    }
 }

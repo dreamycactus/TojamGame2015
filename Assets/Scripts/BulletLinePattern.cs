3e1977b2c145bf290d1c00e047a0daf38c297111
Alex <xander.zaranek@gmail.com>
2015-05-02 18:20:20 -0400

    Made cameras have a horizontal split

--- Assets/Scripts/BulletLinePattern.cs
+++ Assets/Scripts/BulletLinePattern.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 class BulletLinePattern : BulletPattern {
 	public float Period;
 	private float m_cooldown;
-	private float smallCooldown = 0.1f;
+	private float smallCooldown;
 	public int burstCount;
 	private int burstIndex = 0;
 	public float Speed;

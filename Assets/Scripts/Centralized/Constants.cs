8fde83f013034b5a9aedd8a4c1279793b898cd43
dreamycactus <hello_great_man@yahoo.ca>
2015-05-02 12:19:36 -0400

    basic emitter

--- Assets/Scripts/Centralized/Constants.cs
+++ Assets/Scripts/Centralized/Constants.cs
@@ -8,7 +8,9 @@ using System.Collections;
 
 public class Constants : MonoBehaviour {
 
-    //example
-    //public const MOVESPEED = 2.0f;
+	//example
+	//public const MOVESPEED = 2.0f;
+	public static float NORMAL_BULLET_SPEED = 10.0f; 
+	public static float NORMAL_BULLET_PERIOD = 1.0f; 
 
 }

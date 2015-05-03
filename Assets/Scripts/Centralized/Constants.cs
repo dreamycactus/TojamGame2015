7e0e268cf4dce8bb2c7545cd9feaa8c8f9ef9cde
Alex <xander.zaranek@gmail.com>
2015-05-02 20:29:45 -0400

    EASY MODE

--- Assets/Scripts/Centralized/Constants.cs
+++ Assets/Scripts/Centralized/Constants.cs
@@ -11,7 +11,7 @@ public class Constants : MonoBehaviour {
 	//example
 	//public const MOVESPEED = 2.0f;
 	public static float NORMAL_BULLET_SPEED = 10.0f; 
-	public static float NORMAL_BULLET_PERIOD = 1.0f;
-	public static float NORMAL_BULLET_SMALL_PERIOD = 0.1f;
+	public static float NORMAL_BULLET_PERIOD = 2.0f;
+	public static float NORMAL_BULLET_SMALL_PERIOD = 0.3f;
 	public static float ENEMY_DETECTION_RADIUS = 20;
 }

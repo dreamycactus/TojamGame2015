428ecbd3abc314491a40c37527445af60e87f2ff
Adam Bysice <adambysice@gmail.com>
2015-05-03 00:12:00 -0400

    ButtSmoke

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,49 @@
+﻿/*
+ *
+ *
+ */
+using UnityEngine;
+using System.Collections;
+
+public class SelfDestruct : MonoBehaviour {
+
+	#region Public Variables
+    private float timer = 0.3f;
+    #endregion
+
+    #region Protected Variables
+    #endregion
+
+    #region Private Variables
+    
+    #endregion
+
+    #region Accessors
+    #endregion
+
+    #region Unity Defaults
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+            Destroy(gameObject);
+	}
+
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Protected Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
